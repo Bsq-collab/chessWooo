9517a06ab8380cf327d589f94ce4e65f41825b89
Bayan Berri <bberri@stuy.edu>
2017-04-12 20:39:30 -0400

    tostring, populate

---
+++ Chess.java
@@ -0,0 +1,37 @@
+public class Chess{
+    public String[][] board;
+
+    public String[][] whitePieces= new String[2][8];
+    public String[][] blackPieces= new String[2][8];
+
+    public Chess(){
+	board= new String[8][8];
+	whitePieces={{"rook","knight","bishop","queen","king","bishop","knight","rook"},
+		     {"pawn","pawn","pawn","pawn","pawn","pawn","pawn","pawn"}};
+	blackPieces={ {"rook","knight","bishop","queen","king","bishop","knight","rook"},
+		      {"pawn","pawn","pawn","pawn","pawn","pawn","pawn","pawn"} };			     	
+    }
+    public String[][] setUpWhites(){
+	for(int rowctr=0;rowctr<2;rowctr++){
+	    for(int colctr=0;colctr<8;colctr++){
+		board[rowctr][colctr]=whitePieces[rowctr][colctr];
+	    }
+	}
+	return board;
+    }
+    public String[][] setUpBlacks(){
+	for(int rowctr=8;rowctr>6;rowctr--){
+	    for(int colctr=0;colctr<8;colctr++){
+		board[rowctr][colctr]=blackPieces[rowctr][colctr];
+	    }
+	}
+	return board;
+    }
+    public void toString(){
+	for(int row=0;row<8;row++){
+	    for(int col=0;col<8;col++){
+		System.out.println(board[row][col]);
+	    }
+	}
+    }
+}
--- Piece.java
+++ Piece.java
@@ -2,8 +2,9 @@ public abstract class Piece{
     protected boolean alive;
     protected boolean isWhite;
 
-    public void Piece(boolean isW){
+    public Piece(boolean isW){
 	isWhite=isW;//if isW is true then the thing is white.
 	alive=true;
     }
+    
 }
